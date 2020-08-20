b0023dc1db53119aecac5eb3f3790509d5b0b078
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-20 01:30:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(dwarfs) # code an argument here
   dwarfs.each_with_index do |name, i|
   puts #{i=1}. #{dwarfs}"
 end
+end 
 
 def summon_captain_planet# code an argument here
   # Your code here
