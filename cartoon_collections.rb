f47ef8dd2f0fbc397a791e794fc832500bb7833d
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-20 02:00:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -43,7 +43,7 @@ cheese_types = ["cheddar", "gouda", "camembert"]
 snacks = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = nu_array
     array.find do |type|
     cheese_types.include?(type)
   end 
