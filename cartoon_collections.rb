9ff5b8f818ad4d5cd3690fd447db03eb23244ca6
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-20 01:42:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,16 @@
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
-def roll_call_dwarves(names) # code an argument here
-  names = []
-  names.each_with_index do |name, i|
-  puts #{i=1}. #{dwarfs}"
+def roll_call_dwarves(array)
+  i = 0 
+  
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
 end
-end 
+
+roll_call_dwarves(dwarves)
+
 
 def summon_captain_planet# code an argument here
   # Your code here
